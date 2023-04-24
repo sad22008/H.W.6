0a27a670f84fd61f3a930e997717dd7b46affdc8
Pavel <sad22008@yandex.ru>
2023-04-24 16:45:43 +0300

    Добавили файлы

---
+++ Задача 23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке 
+// от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void che(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m}, ");
+    }
+    che(m + 1, n);
+
+}
+che(m, n);
---
+++ Задача 24/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+//  натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa(int m, int n)
+{
+    if (m == n)
+        return n;
+    else return m + summa(m + 1, n);
+}
+Console.Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m, n)}");
